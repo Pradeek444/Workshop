4cf4e87520919c3b97dfdc0acd2bab75a7e7890f
Pradeek444 <113882354+Pradeek444@users.noreply.github.com>
2023-03-08 12:31:50 +0530

    Delete Backend/patient directory

--- Backend/patient/src/main/java/com/pradeek/patient/repository/PatientRepo.java
+++
@@ -1,9 +0,0 @@
-package com.pradeek.patient.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.pradeek.patient.models.Patient;
-
-public interface PatientRepo extends JpaRepository<Patient,Integer> {
-
-}
